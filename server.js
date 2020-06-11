db195f41c84432b425b45658ae7808fb97d80709
Kathryn wade <kathrynwade@Kathryns-MacBook.local>
2020-06-10 19:35:09 -0700

    did not save last commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "employees"
+    database: "employee_trackerDB"
 });
 
 connection.connect();
